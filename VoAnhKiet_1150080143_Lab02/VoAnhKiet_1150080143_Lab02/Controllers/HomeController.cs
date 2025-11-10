4815be3e1901fc5b2c1d00f73473a1bf14fdc796
kietvo5924 <kietcun100@gmail.com>
2025-11-10 14:57:39 +0700

    update

---
+++ VoAnhKiet_1150080143_Lab02/VoAnhKiet_1150080143_Lab02/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VoAnhKiet_1150080143_Lab02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VoAnhKiet_1150080143_Lab02/VoAnhKiet_1150080143_Lab02/Controllers/TableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VoAnhKiet_1150080143_Lab02.Controllers
+{
+    public class TableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
